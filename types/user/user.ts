bec34532cf8a39dc891708132ccd22e7d82a3739
Rafal Szczepanik <rafal.szczepanik@grupawp.pl>
2022-12-13 20:13:49 +0100

    Fixes after CR. Remove unnecessary string from the export path. Change the login field to email in the interface.

--- types/user/user.ts
+++ types/user/user.ts
@@ -1,12 +1,11 @@
 export interface UserLoginData {
     id?: string;
-    login: string;
+    email: string;
     password: string;
 }
 
 export interface UserRegistrationData extends UserLoginData {
     name: string;
     surname: string;
-    email: string;
     repeatPassword: string;
 }
