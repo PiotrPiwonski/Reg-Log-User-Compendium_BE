dde6dc24ae17a813f4571acc4520e8016f783017
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2022-12-21 19:40:55 +0100

    Add password hashing in register

--- routers/user.ts
+++ routers/user.ts
@@ -1,31 +1,27 @@
-import {Request, Router} from "express";
-import { Role } from "../types";
-import {UserRecord} from "../records/user.record";
-import {ValidationError} from "../utils/errors";
-import {UserData} from "../types";
+import { Request, Router } from "express";
+import * as bcrypt from "bcrypt";
+import { UserRecord } from "../records/user.record";
+import { ValidationError } from "../utils/errors";
+import { UserData } from "../types";
 
 export const userRouter = Router();
 
-userRouter.post(
-  "/login",
-  (req: Request<{}, string, UserData>, res, next) => {
-    const { email, password } = req.body;
+userRouter.post("/login", (req: Request<{}, string, UserData>, res, next) => {
+  const { email, password } = req.body;
 
-    res.send("Login route");
-  }
-);
+  res.send("Login route");
+});
 
 userRouter.post(
   "/register",
-   async (req: Request<{}, { id: string} , UserData>, res, next) => {
-
-    if (await UserRecord.getUserByEmail(req.body.email)) {
-           throw new ValidationError('User about this email already exists.')
+  async (req: Request<{}, { id: string }, UserData>, res, next) => {
+    const { email, password } = req.body;
+    if (await UserRecord.getUserByEmail(email)) {
+      throw new ValidationError("User about this email already exists.");
     }
-    const newUser = new UserRecord(req.body);
-
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserRecord({ email, password: hashedPassword });
     const id = await newUser.createUser();
-
 
     res.json({ id });
   }
