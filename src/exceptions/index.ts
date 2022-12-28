9f45c7d99c7fa31514bd94777cc9328859656dfa
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2022-12-28 13:19:28 +0100

    Add WrongCredentialsException

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -1,3 +1,4 @@
 export * from './http.exception';
 export * from './validation.exception';
 export * from './user-with-that-email-already-exists.exception';
+export * from './wrong-credentials.exception';
