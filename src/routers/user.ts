d87dccf7570f96dfb83d39b9e8b0f69fc8364538
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2022-12-28 13:39:51 +0100

    Change errors classes

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -2,7 +2,7 @@ import { Request, Router } from 'express';
 import * as bcrypt from 'bcrypt';
 import { UserRecord } from '../records/user.record';
 import { UserData } from '../types';
-import { ValidationException } from '../exceptions';
+import { HttpException, UserWithThatEmailAlreadyExistsException, WrongCredentialsException } from '../exceptions';
 
 export const userRouter = Router();
 
@@ -11,16 +11,16 @@ userRouter.post(
   async (req: Request<unknown, { user: { id: string; token: string } }, UserData>, res, next) => {
     const { email, password } = req.body;
     if (!email || !password) {
-      throw new Error('Please include email and password.');
+      throw new HttpException(400, 'Please include email and password.');
     }
 
     const user = await UserRecord.getUserByEmail(email);
     if (!user) {
-      //TODO error status:401, message: "Wrong credentials provided"
+      throw new WrongCredentialsException();
     }
     const isMatched = await bcrypt.compare(password, user.password);
     if (!isMatched) {
-      //TODO error status:401, message: "Wrong credentials provided"
+      throw new WrongCredentialsException();
     }
 
     //TODO implementation Login Token
@@ -35,7 +35,7 @@ userRouter.post('/register', async (req: Request<unknown, { id: string }, UserDa
     throw new Error('Please include email and password.');
   }
   if (await UserRecord.getUserByEmail(email)) {
-    throw new ValidationException('User about this email already exists.');
+    throw new UserWithThatEmailAlreadyExistsException(email);
   }
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new UserRecord({ email, password: hashedPassword });
