c32946eab36213af4ae4dd6c0ac25946b3c2bddb
Lenovo <piwonskipiotrp>
2023-01-03 17:29:21 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import { userRouter } from './routers/user';
 import { errorMiddleware } from './middleware';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  }),
+);
 
 app.use(express.json());
 
