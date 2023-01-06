27bab630757a8c6b0c731a841b2447354e6dfdcf
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-06 07:16:44 +0100

    Add AuthenticationTokenMissingException

--- src/exceptions/index.ts
+++ src/exceptions/index.ts
@@ -2,3 +2,4 @@ export * from './http.exception';
 export * from './validation.exception';
 export * from './user-with-that-email-already-exists.exception';
 export * from './wrong-credentials.exception';
+export * from './authentication-token-missing.exception';
