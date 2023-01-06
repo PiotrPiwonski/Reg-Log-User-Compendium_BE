65248ed3dc3fb05b96a067f07f389e5be04bec79
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-06 06:45:40 +0100

    Add a token to the user's login method

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,12 +1,14 @@
 import { Request, Router } from 'express';
 import * as bcrypt from 'bcrypt';
 import { UserRecord } from '../records/user.record';
-import { UserLoginReq, UserLoginRes, UserRegisterReq, UserRegisterRes } from '../types';
+import { UserLoginReq, UserRegisterReq, UserRegisterRes } from '../types';
 import { HttpException, UserWithThatEmailAlreadyExistsException, WrongCredentialsException } from '../exceptions';
+import { createToken } from '../auth/token';
+import { createAuthorizationCookie } from '../utils/cookie';
 
 export const userRouter = Router();
 
-userRouter.post('/login', async (req: Request<unknown, UserLoginRes, UserLoginReq>, res, next) => {
+userRouter.post('/login', async (req: Request<unknown, { success: boolean }, UserLoginReq>, res, next) => {
   const { email, password } = req.body;
   if (!email || !password) {
     throw new HttpException(400, 'Please include email and password.');
@@ -21,15 +23,12 @@ userRouter.post('/login', async (req: Request<unknown, UserLoginRes, UserLoginRe
     throw new WrongCredentialsException();
   }
 
-  //TODO implementation Login Token
+  const tokenData = createToken(user);
 
-  const loggedUser = {
-    id: user.id,
-    email: user.email,
-    role: user.role,
-  };
-
-  res.status(200).json(loggedUser);
+  res
+    .setHeader('Set-Cookie', [createAuthorizationCookie(tokenData)])
+    .status(200)
+    .json({ success: true });
 });
 
 userRouter.post('/register', async (req: Request<unknown, UserRegisterRes, UserRegisterReq>, res, next) => {
