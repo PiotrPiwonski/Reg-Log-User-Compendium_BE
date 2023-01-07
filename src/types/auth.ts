31d09260316b0faece24196cc2e69ec79a5a6ffa
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-07 12:04:52 +0100

    Refactor code: move RequestWithUser interfaces to auth.ts

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,7 +1,13 @@
+import { Request } from 'express';
+import { UserEntity } from './user';
+
 export interface JwtPayload {
   id: string;
 }
 export interface TokenData {
   accessToken: string;
   expiresIn: number;
+}
+export interface RequestWithUser extends Request {
+  user: UserEntity;
 }
