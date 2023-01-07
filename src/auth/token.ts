9e930e4ac0e3dcd7eaf7e0351241159b851bcd80
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-07 14:20:54 +0100

    Change createAccessToken: add JWT_ACCESS_TOKEN_EXPIRATION_TIME and add error handling with try/catch

--- src/auth/token.ts
+++ src/auth/token.ts
@@ -2,17 +2,23 @@ import { sign } from 'jsonwebtoken';
 import { v4 as uuid } from 'uuid';
 import { UserRecord } from '../records/user.record';
 import { JwtPayload, TokenData } from '../types';
+import { HttpException } from '../exceptions';
 
 export const createAccessToken = (currentToken: string): TokenData => {
-  const expiresIn = 60 * 60;
+  const expiresIn = Number(process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME);
   const jwtSecretKey = process.env.JWT_SECRET_KEY;
   const payload: JwtPayload = {
     id: currentToken,
   };
-  return {
-    expiresIn,
-    accessToken: sign(payload, jwtSecretKey, { expiresIn }),
-  };
+  try {
+    const accessToken = sign(payload, jwtSecretKey, { expiresIn });
+    return {
+      expiresIn,
+      accessToken,
+    };
+  } catch (error) {
+    throw new HttpException(error.statusCode, error.message);
+  }
 };
 
 export const generateCurrentToken = async (user: UserRecord): Promise<string> => {
