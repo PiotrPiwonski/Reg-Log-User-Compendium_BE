6c89c827ae15d854385760eee6be40b041bf2bd2
Rafal Szczepanik <rafal.szczepanik@grupawp.pl>
2023-01-23 08:26:49 +0100

    Create refresh middleware.

---
+++ src/middleware/refresh.middleware.ts
@@ -0,0 +1,40 @@
+import { RequestHandler } from 'express';
+import { verify } from 'jsonwebtoken';
+import { CookiesNames, RefreshJwtPayload } from '../types';
+import { UserRecord } from '../records/user.record';
+import { RefreshTokenMissingException, WrongRefreshTokenException } from '../exceptions';
+import { checkHash } from '../utils';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      user?: UserRecord;
+    }
+  }
+}
+export const refreshMiddleware: RequestHandler<unknown> = async (req, res, next) => {
+  const cookies = req.cookies;
+  const refreshToken = cookies[CookiesNames.REFRESH];
+
+  if (cookies && refreshToken) {
+    try {
+      const jwtSecretKey = process.env.REFRESH_JWT_SECRET_KEY;
+      const verificationRes = verify(refreshToken, jwtSecretKey) as RefreshJwtPayload;
+      const user = await UserRecord.getUserById(verificationRes.userId);
+      if (!user) {
+        next(new RefreshTokenMissingException());
+      }
+      const isMatched = await checkHash(refreshToken, user.refreshToken);
+      if (!isMatched) {
+        next(new WrongRefreshTokenException());
+      }
+      req.user = user;
+      next();
+    } catch (e) {
+      next(new WrongRefreshTokenException());
+    }
+  } else {
+    next(new RefreshTokenMissingException());
+  }
+};
