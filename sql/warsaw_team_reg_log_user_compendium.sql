aef4fe482565fcfd0068736f4274e06c1845bac3
Rafal Szczepanik <rafal.szczepanik@grupawp.pl>
2023-01-23 20:21:25 +0100

    Add comma to create table line.

--- sql/warsaw_team_reg_log_user_compendium.sql
+++ sql/warsaw_team_reg_log_user_compendium.sql
@@ -33,7 +33,7 @@ CREATE TABLE `user` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` enum('1','2','3') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1',
-  `currentToken` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `currentToken` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `refreshToken` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
