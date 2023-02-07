64ecdbd197cda19e3a4f6f42b09c0eedfda37b66
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-07 13:20:30 +0100

    Fix API error response to match 'ClientApiResponse' type in errorMiddleware

--- src/middleware/errors.middleware.ts
+++ src/middleware/errors.middleware.ts
@@ -1,13 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from '../exceptions';
+import { ClientApiResponse } from '../types';
 
 export const errorMiddleware = (err: HttpException, req: Request, res: Response, next: NextFunction): void => {
-  console.log(err);
-
   const status = err.status || 500;
   const message = err.message || 'Something went wrong';
-  res.status(status).json({
+
+  console.log({
     status,
     message,
+    timestamp: new Date().toISOString(),
+    path: req.url,
   });
+
+  res.status(status).json({
+    ok: false,
+    status,
+    error: message,
+  } satisfies ClientApiResponse<null>);
 };
